96ce8c6cdf9708a097665f09b83fddaf3770e6d6
cbmdk <cbm@cbm.dk>
2021-02-23 12:20:33 +0100

    Add project files.

---
+++ TheMonarchs/Server/Controllers/MonarchsController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.WebEncoders.Testing;
+using TheMonarchs.Shared;
+
+namespace TheMonarchs.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class MonarchsController : Controller
+    {
+        private IMemoryCache _cache;
+
+        public MonarchsController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public ActionResult GetMonarchs()
+        {
+            var data = _cache.Get<List<Monarch>>("monarchs");
+            return Ok(data);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult Firstnames()
+        {
+            var firstnames = new List<char>();
+            var data = _cache.Get<List<Monarch>>("monarchs");
+            firstnames = data.SelectMany(x => x.nm).ToList();
+            return Ok(firstnames);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult Number()
+        {
+            var data = _cache.Get<List<Monarch>>("monarchs");
+            return Ok(data.Count.ToString());
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult LongestRule()
+        {
+            var data = _cache.Get<List<Monarch>>("monarchs");
+            var rulingYears = new List<Monarch>();
+            foreach (var entry in data)
+            {
+                int years = 0;
+                if (entry.yrs.Contains('-'))
+                {
+                    var validatedYears = CheckYrsValidity(entry.yrs);
+                    int[] year = Array.ConvertAll(validatedYears.Split('-'), int.Parse);
+                    var item = new Monarch();
+                    item = entry;
+                    item.rule = year[1] - year[0];
+                    rulingYears.Add(item);
+                }
+                else
+                {
+                    var item = entry;
+                    item.rule = 0;
+                    rulingYears.Add(item);
+                }
+            }
+            
+            return Ok(rulingYears.OrderByDescending(r=>r.rule).First());
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+
+        public ActionResult LongestRulingHouse()
+        {
+            var data = _cache.Get<List<Monarch>>("monarchs");
+            var houses = data.GroupBy(house => house.hse);
+            var rulingHouses = new List<House>();
+            var monarchs = new List<Monarch>();
+            foreach (var house in houses)
+            {
+                foreach (var entry in house)
+                {
+                    int years = 0;
+                    if (entry.yrs.Contains('-'))
+                    {
+                        var validatedYears = CheckYrsValidity(entry.yrs);
+                        int[] year = Array.ConvertAll(validatedYears.Split('-'), int.Parse);
+                        var item = new Monarch();
+                        item = entry;
+                        item.rule = item.rule = year[1] - year[0];
+                        monarchs.Add(item);
+                    }
+                }
+                rulingHouses.Add(new House
+                {
+                    HouseName = house.Key,
+                    Monarchs = monarchs
+                });
+            }
+            return Ok(houses);
+        }
+
+        private string CheckYrsValidity(string data)
+        {
+            var index = data.IndexOf('-');
+            var left = data.Substring(0, index);
+            var right = data.Substring(index + 1);
+            if (right == "")
+            {
+                right = DateTime.Now.Year.ToString();
+                return $"{left}-{right}";
+            }
+            return data;
+        }
+
+    }
+}
---
+++ TheMonarchs/Shared/Monarch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMonarchs.Shared
+{
+    public class Monarch
+    {
+        public int id { get; set; }
+        public string nm { get; set; }
+        public string cty { get; set; }
+        public string hse { get; set; }
+        public string yrs { get; set; }
+
+        public int rule { get; set; }
+    }
+
+    public class House
+    {
+        public string HouseName { get; set; }
+        public List<Monarch> Monarchs { get; set; }
+    }
+}
